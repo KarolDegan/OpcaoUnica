8d5684b27af8a975ff07cfddcdbd63fbe786183d
Karoline Oliveira Degan <karol.degan@hotmail.com>
2026-06-24 20:59:56 -0300

    ajustes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
-from dotenv import load_dotenv
+# from flask_cors import CORS
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 from routes import *
 
